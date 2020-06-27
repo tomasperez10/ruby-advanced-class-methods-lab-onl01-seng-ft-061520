3227250e738802ce80ee9af3099f8d57eead1063
Tomas Perez <tomasitopg6@gmail.com>
2020-06-27 00:56:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name_of_song)
-    found_song = self.all.find {|i| 
+    found_song = self.all.find {|i| i.name == 
   end
 
   def self.all
